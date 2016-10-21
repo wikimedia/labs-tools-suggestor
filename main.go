42215a88392d72c4786f4752ae5f425fe1676ea3
Arlo Breault <arlolra@gmail.com>
2016-10-21 12:41:32 -0700

    Get CSRF token

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ var callbackParams = map[string]string{
 func (c *Context) Callback(w http.ResponseWriter, r *http.Request) {
 	token, secret, err := getToken(r, "oauthreqtoken")
 	if err != nil {
-		http.Error(w, "Token not found.", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	requestToken := &oauth.RequestToken{Token: token, Secret: secret}
@@ -199,6 +199,65 @@ func (c *Context) ListPendingEdits() ([]Post, error) {
 	return pending, nil
 }
 
+type RespTokens struct {
+	CSRFToken string `json:"csrftoken"`
+}
+
+type RespQuery struct {
+	Tokens RespTokens
+}
+
+type Resp struct {
+	Query RespQuery
+}
+
+func (c *Context) Approve(w http.ResponseWriter, r *http.Request) {
+	token, secret, err := getToken(r, "oauthtoken")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	accessToken := &oauth.AccessToken{Token: token, Secret: secret}
+	client, err := c.consumer.MakeHttpClient(accessToken)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	req, err := http.NewRequest("GET", c.conf.WikiUrl+"/api.php", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	q := req.URL.Query()
+	q.Add("action", "query")
+	q.Add("meta", "tokens")
+	q.Add("type", "csrf")
+	q.Add("format", "json")
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if resp.Body == nil {
+		http.Error(w, "No body got.", http.StatusBadRequest)
+		return
+	}
+	var p Resp
+	if err := json.NewDecoder(resp.Body).Decode(&p); err != nil {
+		str := fmt.Sprintf("Cannot decode got json: %s", err.Error())
+		http.Error(w, str, http.StatusBadRequest)
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Println(p.Query.Tokens.CSRFToken)
+	w.WriteHeader(http.StatusOK)
+}
+
 func (c *Context) CompileTemplates(templates []string) {
 	base := "public/"
 	layout := template.Must(template.ParseFiles(path.Join(base, "layout.html")))
@@ -255,6 +314,7 @@ func main() {
 	http.HandleFunc("/callback", context.Callback)
 	http.HandleFunc("/post", context.Post)
 	http.HandleFunc("/pending", context.Pending)
+	http.HandleFunc("/approve", context.Approve)
 
 	address := conf.Address
 	// Prefer, for tools labs
